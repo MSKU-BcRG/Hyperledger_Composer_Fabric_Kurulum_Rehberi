2eaff5792df61e1166f626e3f1228732b8ad27b4
7aycey7 <ayseceydaolmez@gmail.com>
2018-07-09 14:59:26 +0300

    logic.js

---
+++ logic.js
@@ -0,0 +1,10 @@
+/**
+ * Track the trade of a commodity from one trader to another
+ * @param {org.example.mynetwork.Trade} trade - the trade to be processed
+ * @transaction
+ */
+async function tradeCommodity(trade) {
+    trade.commodity.owner = trade.newOwner;
+    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
+    await assetRegistry.update(trade.commodity);
+}
